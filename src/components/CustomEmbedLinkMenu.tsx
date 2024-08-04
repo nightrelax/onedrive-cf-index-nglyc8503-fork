cf843f2659b8bc0792f5612861d17a3f815942a7
nightrelax <1840688+nightrelax@users.noreply.github.com>
2024-08-04 09:42:01 +0700

    Update CustomEmbedLinkMenu.tsx

--- src/components/CustomEmbedLinkMenu.tsx
+++ src/components/CustomEmbedLinkMenu.tsx
@@ -105,6 +105,10 @@ export default function CustomEmbedLinkMenu({
                   value={`${getBaseUrl()}/api/raw?path=${readablePath}${hashedToken ? `&odpt=${hashedToken}` : ''}`}
                 />
                 <LinkContainer
+                  title={'Web1S'}
+                  value={`${getBaseUrl()}/api/raw?path=${readablePath}${hashedToken ? `&odpt=${hashedToken}` : ''}`}
+                />
+                <LinkContainer
                   title={'URL encoded'}
                   value={`${getBaseUrl()}/api/raw?path=${path}${hashedToken ? `&odpt=${hashedToken}` : ''}`}
                 />
